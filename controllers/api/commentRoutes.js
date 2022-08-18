e4efa38fae927230b5058f807a1c5c11dfdfdc9b
fuaus <kotingfunk@gmail.com>
2022-08-18 11:04:03 -0400

    controllers/api/commentRoutes.js: complete CRUD operations

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,28 +1,82 @@
 const router = require('express').Route();
+const { json } = require('express');
 const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', withAuth, async (req,res)=>{
+router.get('/', async (req,res)=>{
     try{
         const commentData = await Comment.findAll({
+          include: [
+            User,
+            Post
+          ]
+        });
 
-        })
+        res.json(commentData);
     }
     catch (err) {
-        
+        res.status(500).json(err);
     }
-})
+});
 
-router.post('/', withAuth, async (req, res) => {
-  try {
-    const newProject = await Comment.create({
-      ...req.body,
-      user_id: req.session.user_id,
+router.get('/:id', async, (req,res) => {
+  try{
+    const commentData = await Comment.findByPk(req.params.id, {
+      include: [
+        User,
+        Post
+      ]
     });
 
-    res.status(200).json(newProject);
-  } catch (err) {
-    res.status(400).json(err);
+    res.json(commentData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', withAuth, async, (req,res) => {
+  try {
+    const newComment = await Comment.create({
+      body: req.body.comment,
+      user_id: req.session.user_id,
+      post_id: req.session.post_id
+    });
+
+    res.json(newComment);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', withAuth, async, (req,res) => {
+  try {
+    const updateComment = await Comment.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    
+    res.json(updateComment);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async, (req,res) => {
+  try{ 
+    const delComment = await Comment.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    res.json(delComment);
+  }
+  catch (err) {
+    res.status(500).json(err);
   }
 });
 
